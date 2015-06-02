5288cd60858beff2ed038bb5f8d3da4dcd13b73c
Benjamin Landers <benjaminrlanders@gmail.com>
2015-06-02 00:49:03 -0700

    finished giphy (mostly)

--- baton/giphy/giphy.go
+++ baton/giphy/giphy.go
@@ -11,17 +11,34 @@ type Giphy struct {
 }
 
 func (e Giphy) RunCommand(cmd string, body interface{}, resp chan<- interface{}) error {
-	if cmd != "search" {
-		return errors.New("unknown command: " + cmd)
+	var url string
+	switch cmd {
+	  case "search":
+	  	query := body.(map[string]interface{})["q"].(string)
+	    url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+query
+	  case "getbyid":
+	    query := body.(map[string]interface{})["id"].(string)
+	    url = "http://api.giphy.com/v1/gifs/"+query+"?api_key=dc6zaTOxFJmzC"
+	  case "getbyids":
+	    query := body.(map[string]interface{})["ids"].(string)
+	    url = "http://api.giphy.com/v1/gifs?api_key=dc6zaTOxFJmzC&ids="+query
+	  case "translate":
+	    query := body.(map[string]interface{})["term"].(string)
+	    url = "http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s="+query
+	  case "random":
+	    query := body.(map[string]interface{})["tags"].(string)
+	    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tags="+query
+	  case "trending":
+	    url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
+	  default:
+	    return errors.New("unknown command: " + cmd)
 	}
-	query := body.(map[string]interface{})["q"].(string)
-	log.Println(query);
+	log.Println(url)
 	var tmp bytes.Buffer
-  if _, err := http.Get(&tmp, "http://api.giphy.com/v1/gifs/search?q="+query+"&api_key=dc6zaTOxFJmzC"); err != nil {
+  if _, err := http.Get(&tmp, url); err != nil {
       log.Fatalf("could not fetch: %v", err)
       return nil
   }
-  log.Println(tmp.String())
   var out interface{}
 	if err := json.Unmarshal(tmp.Bytes(), &out); err != nil {
 		log.Println("nu")
